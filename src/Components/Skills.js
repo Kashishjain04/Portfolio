ff38e4fc9c05da66a9ad29e187edeb9a7d987fd3
Kashish Jain <jainabhishek7204@gmail.com>
2021-05-30 10:09:55 +0530

    Added links to documentation to skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -5,30 +5,38 @@ import SkillCard from "./SmallComponents/SkillCard";
 
 const Skills = () => (
   <div className="skills section" id="skills">
-    <h2 className="section-title" >Skills</h2>
+    <h2 className="section-title">Skills</h2>
 
     <div className="skills__container bd-grid">
-      <h2 className="skills__subtitle" data-aos="fade-down">Languages</h2>
+      <h2 className="skills__subtitle" data-aos="fade-down">
+        Languages
+      </h2>
       <div className="skills__list bd-grid">
         {skills.languages.map((lang, index) => (
           <SkillCard
-          key={lang.name}
-          name={lang.name}
-          iconClass={lang.iconClass}
-          data-aos="fade-down"
-          data-aos-duration={400+(1000/skills.languages.length)*index}
+            key={lang.name}
+            name={lang.name}
+            iconClass={lang.iconClass}
+            link={lang.link}
+            data-aos="fade-down"
+            data-aos-duration={400 + (1000 / skills.languages.length) * index}
           />
         ))}
       </div>
-      <h2 className="skills__subtitle" data-aos="fade-down">Technologies</h2>
+      <h2 className="skills__subtitle" data-aos="fade-down">
+        Technologies
+      </h2>
       <div className="skills__list bd-grid">
         {skills.technologies.map((tech, index) => (
           <SkillCard
             key={tech.name}
             name={tech.name}
             iconClass={tech.iconClass}
+            link={tech.link}
             data-aos="fade-down"
-          data-aos-duration={400+(1000/skills.technologies.length)*index}
+            data-aos-duration={
+              400 + (1000 / skills.technologies.length) * index
+            }
           />
         ))}
       </div>
--- src/Components/SmallComponents/SkillCard.js
+++ src/Components/SmallComponents/SkillCard.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-const SkillCard = ({ name, iconClass, ...other }) => {
+const SkillCard = ({ name, iconClass, link, ...other }) => {
   return (
-    <div className="skills__data" {...other}>
-      <i className={`bx skills__icon ${iconClass}`}></i>
-      <span className="skills__name">{name}</span>
-    </div>
+    // <div className="skills__data" {...other}>
+    link ? (
+      <a
+        className="skills__data"
+        {...other}
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className={`bx skills__icon ${iconClass}`}></i>
+        <span className="skills__name">{name}</span>
+      </a>
+    ) : (
+      <div className="skills__data" {...other}>
+        <i className={`bx skills__icon ${iconClass}`}></i>
+        <span className="skills__name">{name}</span>
+      </div>
+    )
+    // </div>
   );
 };
 
